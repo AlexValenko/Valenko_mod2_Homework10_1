0266fe556258ccb99ffa6e91dc7d4164385d316f
Alex Valenko <valenko.a.geo@gmail.com>
2026-03-13 12:24:39 +0300

    checked src by linters

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
 
+
 def mask_account_card(user_card_data: str) -> str:
     """Принимает данные счета или карты и маскирует номер"""
     user_data_list = user_card_data.split()
