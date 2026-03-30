44ab40f848c074fe911fe31250b44893c8653c4d
Alex Valenko <valenko.a.geo@gmail.com>
2026-03-30 15:41:38 +0300

    add func filter_by_status

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,12 @@
-def filter_by_state():
+def filter_by_state(input_list: list, current_state: str = "EXECUTED") -> list:
     """Возвращает новый список словарей, содержащий только те словари, у которых ключ
-    state соответствует указанному значению"""
-    pass
+    state соответствует указанному значению. По умолчанию 'EXECUTED'"""
+    output_list = []
+    for i in input_list:
+        if "state" in i:
+            if i["state"] == current_state:
+                output_list.append(i)
+    return output_list
 
 
 def sort_by_date():
