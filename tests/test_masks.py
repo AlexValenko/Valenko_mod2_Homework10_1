174e76bc942a4d81326fc229f9b853485c62c070
Alex Valenko <valenko.a.geo@gmail.com>
2026-04-03 16:09:43 +0300

    add docstring & htmlcov

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -16,6 +16,7 @@ def account_number_normal() -> int:
 
 
 def test_get_mask_card_number_normal(card_number_normal: int) -> None:
+    """Проверка работы get_mask_card_number с позитивным исходом"""
     assert get_mask_card_number(card_number_normal) == "7000 79** **** 6361"
 
 
@@ -26,26 +27,29 @@ def test_get_mask_card_number_normal(card_number_normal: int) -> None:
         ("7000792289606361", "7000 79** **** 6361"),  # Если строка
         ("  1111 1111 1111 1111  ", "1111 11** **** 1111"),  # Если пробелы
         ("1234 5678 123 12", "1234 56** **** 2312"),  # Если 13 цифр
-        ("123", "Некорректный номер карты"),
+        ("123", "Некорректный номер карты"),  # Если слишком короткий номер
         ("", "Некорректный номер карты"),  # Если пустой ввод
     ],
 )
 def test_get_mask_card_number_different_input(card_number: Union[str, int], expected: str) -> None:
+    """Проверка работы get_mask_card_number с различными вариантами входных данных"""
     assert get_mask_card_number(card_number) == expected
 
 
 def test_get_mask_account_normal(account_number_normal: int) -> None:
+    """Проверка работы get_mask_account с позитивным исходом"""
     assert get_mask_account(account_number_normal) == "**4305"
 
 
 @pytest.mark.parametrize(
     "account_number, expected",
     [
-        (73654108430135874305, "**4305"),
-        ("73654108430135874305", "**4305"),
-        ("11 111  111 11111", "**1111"),
-        ("", "Некорректный номер счета"),
+        (73654108430135874305, "**4305"),  # Если integer
+        ("73654108430135874305", "**4305"),  # Если строка
+        ("11 111  111 11111", "**1111"),  # Если пробелы
+        ("", "Некорректный номер счета"),  # Если пустой ввод
     ],
 )
 def test_get_mask_account_different_input(account_number: Union[str, int], expected: str) -> None:
+    """Проверка работы get_mask_account с различными вариантами входных данных"""
     assert get_mask_account(account_number) == expected
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -16,29 +16,37 @@ def input_list_test_data() -> List[Dict]:
 
 
 def test_filter_by_state(input_list_test_data: List[Dict]) -> None:
+    """Проверка работы filter_by_state с различными данными"""
+    # Нормальный случай работы, "state" по умолчанию
     assert filter_by_state(input_list_test_data) == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     ]
+    # Нормальный случай работы, "state" по выбору
     assert filter_by_state(input_list_test_data, "CANCELED") == [
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
+    # Ключа "state" нет в словаре
     assert filter_by_state([{"id": 123, "date": "2019-07-03T18:35:29.512364"}, {"id": 111}]) == []
-    assert filter_by_state([]) == []
+    assert filter_by_state([]) == []  # Пустой ввод
 
 
 def test_sort_by_date(input_list_test_data: List[Dict]) -> None:
+    """Проверка работы sort_by_date"""
+    # Нормальный случай работы, направление сортировки по умолчанию
     assert sort_by_date(input_list_test_data) == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     ]
+    # Нормальный случай работы, направление сортировки по выбору
     assert sort_by_date(input_list_test_data, False) == [
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
     ]
+    # Пустой ввод
     assert sort_by_date([]) == []
