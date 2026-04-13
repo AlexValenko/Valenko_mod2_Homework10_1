50b2f34292b56a05f20308bfe222f176d54a7cf3
Alex Valenko <valenko.a.geo@gmail.com>
2026-04-13 09:46:15 +0300

    src/generators and tests/generators done

--- src/generators.py
+++ src/generators.py
@@ -1,120 +1,36 @@
-# Пример входных данных
-transactions = (
-    [
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
-        },
-        {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
-        },
-        {
-            "id": 873106923,
-            "state": "EXECUTED",
-            "date": "2019-03-23T01:09:46.296404",
-            "operationAmount": {
-                "amount": "43318.34",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160"
-        },
-        {
-            "id": 895315941,
-            "state": "EXECUTED",
-            "date": "2018-08-19T04:27:37.904916",
-            "operationAmount": {
-                "amount": "56883.54",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод с карты на карту",
-            "from": "Visa Classic 6831982476737658",
-            "to": "Visa Platinum 8990922113665229"
-        },
-        {
-            "id": 594226727,
-            "state": "CANCELED",
-            "date": "2018-09-12T21:27:25.241689",
-            "operationAmount": {
-                "amount": "67314.70",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Visa Platinum 1246377376343588",
-            "to": "Счет 14211924144426031657"
-        }
-    ]
-)
+from collections.abc import Generator
 
-def filter_by_currency(transactions_data, currency):
+
+def filter_by_currency(transactions_data: list[dict], currency: str) -> Generator:
     """Функция принимает на вход список словарей, представляющих транзакции.
     Функция возвращает итератор, который поочередно выдает транзакции,
     где валюта операции соответствует заданной"""
     if not transactions_data:
         yield []
+    if not any(x["operationAmount"]["currency"]["code"] == currency for x in transactions_data):
+        yield "No transactions"
     try:
         yield from filter(lambda x: x["operationAmount"]["currency"]["code"] == currency, transactions_data)
     except KeyError:
-        print("key not found")
+        yield "key not found"
 
 
-# Пример использования функции filter_by_currency
-usd_transactions = filter_by_currency(transactions, "USD")
-for _ in range(6):
-    try:
-        print(next(usd_transactions))
-    except StopIteration:
-        print("No data available")
-
-def transaction_descriptions(transactions_data):
+def transaction_descriptions(transactions_data: list[dict]) -> Generator:
     """Принимает список транзакций и возвращает описание каждой операции по очереди"""
     if not transactions_data:
         yield []
-    for i in transactions_data:
-        try:
-            yield i["description"]
-        except KeyError:
-            print("key description not found")
+    if not any(x["description"] for x in transactions_data):
+        yield "No transactions"
+    else:
+        for i in transactions_data:
+            try:
+                yield i["description"]
+            except KeyError:
+                yield "key description not found"
 
-# Пример использования функции transaction_descriptions
-descriptions = transaction_descriptions(transactions)
-for _ in range(5):
-    print(next(descriptions))
 
-def card_number_generator(start_card_num = 1, end_card_num = 9999999999999999):
+def card_number_generator(start_card_num: int = 1, end_card_num: int = 9999999999999999) -> Generator:
+    """Принимает начальный и конечный номер диапазона и выдает номера банковских карт в формате XXXX XXXX XXXX XXXX"""
     if 1 <= start_card_num <= end_card_num <= 9999999999999999:
         num = start_card_num
         while num <= end_card_num:
@@ -123,7 +39,3 @@ def card_number_generator(start_card_num = 1, end_card_num = 9999999999999999):
             num += 1
     else:
         yield "Incorrect diapason"
-
-# Пример использования функции card_number_generator
-for card_number in card_number_generator(-1, 10):
-    print(card_number)
