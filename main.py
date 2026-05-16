baf6e14492b41f7e77adc09911615da2bc4fbe1a
Alex Valenko <valenko.a.geo@gmail.com>
2026-05-16 18:33:01 +0300

    added masks_logger in src

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 from src.masks import get_mask_account, get_mask_card_number
 from src.utils import get_fin_transactions
 
-# Тестовая функций для модуля src/masks.py
-def main_masks() -> None:
-    """Печатает в консоль маскированный номер карты и счета"""
-    card_number = 7000792289606361  # пример номера карты
-    account_number = 73654108430135874305  # пример номера счета
-
-    masked_card = get_mask_card_number(card_number)
-    masked_account = get_mask_account(account_number)
-
-    print(f"Маскированный номер карты: {masked_card}")
-    print(f"Маскированный номер счета: {masked_account}")
-
-# Тестовый запуск функции get_fin_transactions из модуля src/utils.py
+# Тестовый запуск
 if __name__ == "__main__":
-    # Нормальная работа
+    # Нормальная работа функций get_fin_transactions из модуля src/utils.py
     example_transactions = get_fin_transactions('data/operations.json')
     print(example_transactions[1])
     # Файл поврежден
     get_fin_transactions('tests/tests_data/invalid_file.json')
+
+    # Тестовый запуск функций из модуля src/masks.py - нормальные данные
+    print(get_mask_card_number(7000792289606361))
+    print(get_mask_account(73654108430135874305))
+    # Тестовый запуск функций из модуля src/masks.py - некорректные данные
+    get_mask_card_number('acb')
+    get_mask_account(0)
