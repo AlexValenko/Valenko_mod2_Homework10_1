282bfbd61b95a85971f7b33460a2036cbf530a2d
Alex Valenko <valenko.a.geo@gmail.com>
2026-05-31 13:43:53 +0300

    added foo process_bank_operations in src.search

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from src.read_csv_excel import get_transaction_from_csv, get_transaction_from_excel
-from src.search import process_bank_search
+from src.search import process_bank_search, process_bank_operations
 # Тестовый запуск
 if __name__ == "__main__":
 
     #Тестовый запуск чтения транзакций из файла csv
     csv_transactions = get_transaction_from_csv(path_csv='data/transactions.csv')
     print(csv_transactions[0])
-    opened_transactions = process_bank_search(data=csv_transactions, search="открыт")
+    opened_transactions = process_bank_search(data=csv_transactions, search="Перевод со счета на счет")
     print(f'Найдено транзакций {len(opened_transactions)}')
     print(opened_transactions[0].get("id"), opened_transactions[0].get("description"))
-    print(opened_transactions[1].get("id"), opened_transactions[1].get("description"))
-    print(opened_transactions[2].get("id"), opened_transactions[2].get("description"))
-    print(opened_transactions[-2].get("id"), opened_transactions[-2].get("description"))
     print(opened_transactions[-1].get("id"), opened_transactions[-1].get("description"))
+
+    current_category = ["Перевод организации", "Открытие вклада", "Перевод со счета на счет"]
+    category_counts = process_bank_operations(data=csv_transactions, categories=current_category)
+    print(category_counts)
 
     #
     # excel_transactions = get_transaction_from_excel(path_xlsx='data/transactions_excel.xlsx')
--- src/search.py
+++ src/search.py
@@ -1,13 +1,24 @@
 import re
+from collections import Counter
 
-def process_bank_search(data:list[dict], search:str)->list[dict]:
+
+def process_bank_search(data: list[dict], search: str) -> list[dict]:
     """Принимает список словарей с данными о банковских операциях и строку поиска,
-     и возвращает список словарей, у которых в описании есть данная строка"""
+    и возвращает список словарей, у которых в описании есть данная строка"""
     filtered_transactions = []
     pattern = re.compile(search, re.IGNORECASE)
     for transaction in data:
-        if re.search(pattern=pattern, string=transaction.get('description')):
+        if re.search(pattern=pattern, string=str(transaction.get("description"))):
             filtered_transactions.append(transaction)
     return filtered_transactions
 
 
+def process_bank_operations(data: list[dict], categories: list) -> dict:
+    """Функция принимает список словарей с данными о банковских операциях и список категорий операций,
+    возвращает словарь, в котором ключи — это названия категорий,
+    а значения — это количество операций в каждой категории."""
+    # Список - сборка из описаний транзакций только выбранных категорий
+    filtered_description = [
+        transaction.get("description") for transaction in data if transaction.get("description") in categories
+    ]
+    return dict(Counter(filtered_description))
